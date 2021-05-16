c616203b66a195b058b15bead3e63d304fe03138
llimacruz <leonardo.cruz@minutrade.com>
2021-05-16 18:40:02 -0300

    Refactor router identification

--- src/check-routes.js
+++ src/check-routes.js
@@ -1,3 +1,31 @@
+const checkRoutes = (params) => {
+  if (!params.app) throw new Error('Express server must be present on app parameter');
+  const allowedUnsafe = params.allowedUnsafe || [];
+
+  let expressRouter = params.app._router.stack.find((element) => element.name === 'router');
+  const router = expressRouter
+    ? expressRouter.handle.stack
+    : params.app._router.stack.filter((element) => element.name === 'bound dispatch');
+
+  const routes = buildRoutes(router, allowedUnsafe);
+
+  return {
+    unsafeCounter: routes.filter((route) => !route.hasAuth && !route.allowedUnsafe).length || 0,
+    allowedUnsafeCounter: routes.filter((route) => !route.hasAuth && route.allowedUnsafe).length || 0,
+    unsafeRoutes: routes.filter((route) => !route.hasAuth && !route.allowedUnsafe).map((route) => route.path),
+    allRoutes: routes
+  };
+};
+
+const buildRoutes = (router, allowedUnsafe) => router.map(element => {
+  return {
+    path: element.route.path,
+    method: getMethod(element.route),
+    hasAuth: !!element.route.stack.find((stackRouteItem) => stackRouteItem.name === 'authenticate'),
+    allowedUnsafe: allowedUnsafe.includes(element.route.path)
+  }
+});
+
 const getMethod = (route) => {
   let method;
   Object.keys(route.methods).forEach((key) => {
@@ -8,43 +36,6 @@ const getMethod = (route) => {
     if (key === 'delete') method = key;
   });
   return method;
-};
-
-const checkRoutes = (params) => {
-  if (!params.app) throw new Error('Express server must be present on app parameter');
-  const allowedUnsafe = params.allowedUnsafe || [];
-
-  const routes = [];
-  const router = params.app._router.stack.find((element) => element.name === 'router');
-  if (router) {
-    router.handle.stack.forEach((element) => {
-      const route = {
-        path: element.route.path,
-        method: getMethod(element.route),
-        hasAuth: !!element.route.stack.find((stackRouteItem) => stackRouteItem.name === 'authenticate'),
-        allowedUnsafe: allowedUnsafe.includes(element.route.path)
-      };
-      routes.push(route);
-    });
-  } else {
-    const rootRouter = params.app._router.stack.filter((element) => element.name === 'bound dispatch');
-    rootRouter.forEach((element) => {
-        const route = {
-          path: element.route.path,
-          method: getMethod(element.route),
-          hasAuth: !!element.route.stack.find((stackRouteItem) => stackRouteItem.name === 'authenticate'),
-          allowedUnsafe: allowedUnsafe.includes(element.route.path)
-        };
-        routes.push(route);
-      });
-  }
-
-  return {
-    unsafeCounter: routes.filter((route) => !route.hasAuth && !route.allowedUnsafe).length || 0,
-    allowedUnsafeCounter: routes.filter((route) => !route.hasAuth && route.allowedUnsafe).length || 0,
-    unsafeRoutes: routes.filter((route) => !route.hasAuth && !route.allowedUnsafe).map((route) => route.path),
-    allRoutes: routes
-  };
 };
 
 module.exports = checkRoutes;
